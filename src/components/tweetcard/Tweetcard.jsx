3be8f6ae7caa363048c7094edd2f8066ccab4478
OlgaMunaieva <asyssync@gmail.com>
2023-05-17 17:07:26 +0300

    added card background

---
+++ src/components/tweetcard/Tweetcard.jsx
@@ -0,0 +1,10 @@
+import { CardBackground, Text } from "./Tweetcard.styled";
+
+export const Tweetcard = () => {
+  return (
+    <CardBackground>
+      <Text> 777 tweets</Text>
+      <Text>100,500 Followers</Text>
+    </CardBackground>
+  );
+};
---
+++ src/components/tweetcard/Tweetcard.styled.js
@@ -0,0 +1,33 @@
+import { styled } from "styled-components";
+
+export const CardBackground = styled.div`
+  width: 380px;
+  height: 460px;
+  /* left: 435px; */
+  /* top: 168px; */
+
+  background: linear-gradient(
+    114.99deg,
+    #471ca9 -0.99%,
+    #5736a3 54.28%,
+    #4b2a99 78.99%
+  );
+  box-shadow: -2.5777px 6.87386px 20.6216px rgba(0, 0, 0, 0.23);
+  border-radius: 20px;
+`;
+
+export const Text = styled.p`
+  /* width: 132px; */
+  /* height: 24px; */
+  /* left: 124px; */
+  /* top: 284px; */
+
+  font-family: "Montserrat";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 24px;
+  text-transform: uppercase;
+
+  color: #ebd8ff;
+`;

e2744501f11b79616e29d1a5c7d7512391faddcc
OlgaMunaieva <asyssync@gmail.com>
2023-05-20 22:58:59 +0300

    before broken)

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,24 @@
+import reactLogo from "../assets/react.svg";
+import logo from "../images/logo.png";
+
 export const Home = () => {
   return (
-    <div>
-      <h1>Trending today</h1>
-    </div>
+    <>
+      <h1>Welcome</h1>
+      <div>
+        <a href="https://vitejs.dev" target="_blank">
+          <img src={logo} className="logo" alt="Vite logo" width={200} />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img
+            src={reactLogo}
+            className="logo react"
+            alt="React logo"
+            width={76}
+            height={22}
+          />
+        </a>
+      </div>
+    </>
   );
 };
---
+++ src/pages/Tweets.jsx
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import { useSelector } from "react-redux";
+import { selectIsLoading, selectUsersWithFollowerOf } from "../redux/selectors";
+import { PER_PAGE } from "../redux/operations";
+import UsersList from "../components/usersList/UsersList";
+import { Button } from "../components/button/Button";
+
+export const Tweets = () => {
+  const [page, setPage] = useState(1);
+  const users = useSelector(selectUsersWithFollowerOf);
+  const isLoading = useSelector(selectIsLoading);
+
+  console.log(page);
+  console.log(users);
+  console.log(isLoading);
+
+  const isShowButton = users.length && !isLoading && !(users.length % PER_PAGE);
+  // users.length && !isLoading && !(users.length % PER_PAGE);
+  const nextPage = () => {
+    setPage((prevPage) => prevPage + 1);
+  };
+
+  return (
+    <>
+      <UsersList page={page} />
+      {isShowButton === true && <Button onClick={nextPage} />}
+    </>
+  );
+};

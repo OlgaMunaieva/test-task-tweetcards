73abe383585dd5d5630823df92c5930cd5afd75a
OlgaMunaieva <asyssync@gmail.com>
2023-05-21 15:05:14 +0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -5,14 +5,14 @@ import "./index.css";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistor, store } from "./redux/store.js";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, HashRouter } from "react-router-dom";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <BrowserRouter basename="/test-task-tweetcards/">
+    <HashRouter basename="/">
       <PersistGate persistor={persistor}>
         <App />
       </PersistGate>
-    </BrowserRouter>
+    </HashRouter>
   </Provider>
 );
